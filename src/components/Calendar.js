0082442e3a2f9bfd6c19ad28fa04bca4eca5960e
Robert Gardunia <robertgardunia@hotmail.com>
2024-02-22 16:01:15 -0800

    initial react/next setup

---
+++ src/components/Calendar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DateCard from './DateCard';
+// import * as util from '../apis/util.js';
+
+const Calendar = (props) => {
+
+}
+
+export default Calendar;
---
+++ src/components/DateCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReleasePromo from './ReleasePromo';
+
+const DateCard = (props) => {
+
+}
+
+export default DateCard;
