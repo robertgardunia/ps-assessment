351b9dc4c1cfede9ca4d579584e09fda4366d5a5
Robert Gardunia <robertgardunia@hotmail.com>
2024-02-25 12:57:00 -0800

    check for url params - NOW switching branches

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,52 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import Image from "next/image";
+
 import styles from "./page.module.css";
+
 import Calendar from "@/components/Calendar";
 
 export default function Home() {
+  // determine month/year from default parameters, defaulting to 01/2024
+  const [calendarMonth, setCalendarMonth] = useState(1);
+  const [calendarYear, setCalendarYear] = useState(2020);
+
+  // check for month/year params
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('month')) {
+    let paramMonth = urlParams('month');
+    let regexMonth = /(1[0-2])|([0-9])/g;
+   
+    if (paramMonth.match(regexMonth)) {
+      setCalendarMonth(paramMonth);
+    }  else {
+      console.log(`month param of ${paramMonth} is invalid`)
+    }   
+  }
+
+  if (urlParams.has('year')) {
+    let paramYear = urlParams('year');
+    let regexYear = /(19[0-9]{2}|2[0-9]{3})/g; 
+   
+    if (paramYear.match(regexYear)) {
+      setCalendarYear(paramYear);
+    } else {
+      console.log(`year param of ${paramYear} is invalid`)
+    }   
+  }
+
+  console.log(`passed date: ${calendarMonth}/${calendarYear} `)
+
   return (
     <main className={styles.main}>
       <div>
         <h1>Upcoming Releases</h1>
       </div>
-      <Calendar></Calendar>      
+      <Calendar
+        calendarMonth={calendarMonth} 
+        calendarYear={calendarYear}
+      />     
     </main>
   );
 }
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,8 +1,17 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import DateCard from './DateCard';
 // import * as util from '../apis/util.js';
 
 const Calendar = (props) => {
+    // setup calendar 
+
+    return (
+        <div class="calendar">
+            <p>calendar</p>
+        </div>
+    )
 
 }
 
